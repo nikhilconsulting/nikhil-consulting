6bf9244e3ec1028f0fa46af1838c1ca2b4cfaccd
Muskan Singh <01-Nik>
2025-08-25 00:19:56 +0530

    on main page

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@
 export default function Home() {
   return (
 <>
-<div className=" text-center mt-10 font-bold text-xl">
+<div className=" bg-white text-center mt-10 font-bold text-xl">
 <p className="text-black " style={{ fontFamily: "var(--font-poppins)" }}>Give your business credibility with a professional email – like name@yourcompany.com – using Google Workspace. Plus, get more storage and premium business features.Compare what you have to what you can get if you upgrade to Google Workspace today. </p> <br />
 <p className="text-black" style={{ fontFamily: "var(--font-lato)" }}>Give your business credibility with a professional email – like name@yourcompany.com – using Google Workspace. Plus, get more storage and premium business features.Compare what you have to what you can get if you upgrade to Google Workspace today. </p> <br />
 <p className="text-black" style={{ fontFamily: "var(--font-roboto)" }}>Give your business credibility with a professional email – like name@yourcompany.com – using Google Workspace. Plus, get more storage and premium business features.Compare what you have to what you can get if you upgrade to Google Workspace today. </p><br />
