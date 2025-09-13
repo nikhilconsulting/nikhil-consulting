85e95e7b693cc31193d5530fa0a836ba68ed7673
Muskan Singh <01-Nik>
2025-09-13 10:49:13 +0530

    cordpolicy mein baseurl change kiya h tb live pe chala h

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,3 +1,6 @@
 // lib/config.js
-// export const baseUrl = 'https://www.wmhindia.com';  
-export const baseUrl = 'http://localhost:3000';
+
+// export const baseUrl = 'http://localhost:3000';
+
+export const baseUrl = 'https://nikhil-consulting.vercel.app';
+
